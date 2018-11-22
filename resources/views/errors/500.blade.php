41b937b8388ae61753dca2ef4c84b733e28df19a
laguhing5221 <2755328439@qq.com>
2018-11-22 19:42:15 +0800

    后台接口，404 ，500页面样式

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -9,8 +9,8 @@
 
   <title>500 Page</title>
 
-  <link href="css/style.css" rel="stylesheet">
-  <link href="css/style-responsive.css" rel="stylesheet">
+  <link href="/Admin/css/style.css" rel="stylesheet">
+  <link href="/Admin/css/style-responsive.css" rel="stylesheet">
 
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
@@ -25,21 +25,21 @@
     <div class="container ">
 
         <section class="error-wrapper text-center">
-            <h1><img alt="" src="images/500-error.png"></h1>
+            <h1><img alt="" src="/Admin/images/500-error.png"></h1>
             <h2>OOOPS!!!</h2>
             <h3>Something went wrong.</h3>
-            <p class="nrml-txt">Why not try refreshing you page? Or you can <a href="#">contact our support</a> if the problem persists.</p>
-            <a class="back-btn" href="index.html"> Back To Home</a>
+            <p class="nrml-txt">尝试点击返回主页面 <a href="#">contact our support</a> if the problem persists.</p>
+            <a class="back-btn" href="/"> Back To Home</a>
         </section>
 
     </div>
 </section>
 
 <!-- Placed js at the end of the document so the pages load faster -->
-<script src="js/jquery-1.10.2.min.js"></script>
-<script src="js/jquery-migrate-1.2.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/modernizr.min.js"></script>
+<script src="/Admin/js/jquery-1.10.2.min.js"></script>
+<script src="/Admin/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="/Admin/js/bootstrap.min.js"></script>
+<script src="/Admin/js/modernizr.min.js"></script>
 
 
 <!--common scripts for all pages-->
