a14f601f52f0efb5e56f7c76d40777a8c612230d
yelonghui <247510703@qq.com>
2018-11-23 09:43:16 +0800

    修改 商品详情 细节

--- resources/views/Home/Single/Single.blade.php
+++ resources/views/Home/Single/Single.blade.php
@@ -17,7 +17,7 @@
                 <ul class="breadcrumbs">
                     <li><a href="/">首页</a></li>
                     <li class="separator">&nbsp;</li>
-                    <li><a href="shop-grid-3.html">商品分类</a></li>
+                    <li><a href="/goods/{{0}}">商品分类</a></li>
                     <li class="separator">&nbsp;</li>
                     <li>商品详情</li>
                 </ul>
@@ -68,15 +68,15 @@
                 <div class="d-flex flex-wrap justify-content-between mb-30">
                     <div class="entry-share">
                     </div>
-                    <form  method="post" action="/homecart">
                     <div class="sp-buttons mt-2 mb-2">
-                        <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="" data-original-title="Whishlist">
-                            <i class="icon-heart"></i>
-                        </button>
-                        <button class="btn btn-primary" data-toast="" data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!"><i class="icon-bag"></i> 添加购物车</button>
-                        <input type="hidden" name="id" value="{{$data->id}}">
-                        {{csrf_field()}}
-                    </form>
+                         <a  class="btn btn-outline-secondary btn-sm " data-toggle="tooltip" title="我喜欢" href="/homewish/{{$data->id}}" style="margin-top: 8px" >
+                        <i class="icon-heart" ></i></a>
+                        <form  method="post" action="/homecart" style="display:inline-block;">
+                            <button class="btn btn-outline-primary btn-sm " data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!" style="margin-top: 8px">添加到购物车</button>
+                            
+                            <input type="hidden" name="id" value="{{$data->id}}">
+                            {{csrf_field()}}
+                            </form>
                     </div>
                 </div>
             </div>
@@ -144,132 +144,7 @@
             </div>
         </div>
         <!-- End Product Tabs -->
-        <!-- Start Related Products -->
-        <h3 class="text-center padding-top-3x mb-30">发布的产品</h3>
-        <div class="owl-carousel" data-owl-carousel='{ "nav": false, "dots": false, "margin": 30, "responsive": {"0":{"items":1},"576":{"items":2},"768":{"items":3},"991":{"items":4},";1200":{"items":4}} }'>
-            <!-- Start Product #1 -->
-            <div class="grid-item">
-                <div class="product-card">
-                    <a class="product-thumb" href="shop-single-3.html">
-                        <img src="/Home/images/shop/products/01.jpg" alt="Product">
-                    </a>
-                    <h3 class="product-title"><a href="shop-single-3.html">iPhone X</a></h3>
-                    <h4 class="product-price">$749.99</h4>
-                    <div class="product-buttons">
-                        <div class="product-buttons">
-                            <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist">
-                                <i class="icon-heart"></i>
-                            </button>
-                            <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">添加到购物侧</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Product #1 -->
-            <!-- Start Product #2 -->
-            <div class="grid-item">
-                <div class="product-card">
-                    <div class="rating-stars">
-                        <i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i>
-                    </div>
-                    <a class="product-thumb" href="shop-single-2.html">
-                        <img src="/Home/images/shop/products/05.jpg" alt="Product">
-                    </a>
-                    <h3 class="product-title"><a href="shop-single-2.html">松下 TX-32</a></h3>
-                    <h4 class="product-price">$949.50</h4>
-                    <div class="product-buttons">
-                        <div class="product-buttons">
-                            <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist">
-                                <i class="icon-heart"></i>
-                            </button>
-                            <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">添加到购物侧</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Product #2 -->
-            <!-- Start Product #3 -->
-            <div class="grid-item">
-                <div class="product-card">
-                    <a class="product-thumb" href="shop-single-3.html">
-                        <img src="/Home/images/shop/products/09.jpg" alt="Product">
-                    </a>
-                    <h3 class="product-title"><a href="shop-single-3.html">索尼 HDR-AS50R</a></h3>
-                    <h4 class="product-price">$700.00</h4>
-                    <div class="product-buttons">
-                        <div class="product-buttons">
-                            <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist">
-                                <i class="icon-heart"></i>
-                            </button>
-                            <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">添加到购物侧</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Product #3 -->
-            <!-- Start Product #4 -->
-            <div class="grid-item">
-                <div class="product-card">
-                    <div class="rating-stars">
-                        <i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i>
-                    </div>
-                    <a class="product-thumb" href="shop-single-2.html">
-                        <img src="/Home/images/shop/products/13.jpg" alt="Product">
-                    </a>
-                    <h3 class="product-title"><a href="shop-single-2.html">HP LaserJet Pro 200</a></h3>
-                    <h4 class="product-price">$249.50</h4>
-                    <div class="product-buttons">
-                        <div class="product-buttons">
-                            <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist">
-                                <i class="icon-heart"></i>
-                            </button>
-                            <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">添加到购物侧</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Product #4 -->
-            <!-- Start Product #5 -->
-            <div class="grid-item">
-                <div class="product-card">
-                    <a class="product-thumb" href="shop-single-3.html">
-                        <img src="/Home/images/shop/products/17.jpg" alt="Product">
-                    </a>
-                    <h3 class="product-title"><a href="shop-single-3.html">Apple Watch 3</a></h3>
-                    <h4 class="product-price">$449.00</h4>
-                    <div class="product-buttons">
-                        <div class="product-buttons">
-                            <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist">
-                                <i class="icon-heart"></i>
-                            </button>
-                            <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">添加到购物侧</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Product #5 -->
-            <!-- Start Product #6 -->
-            <div class="grid-item">
-                <div class="product-card">
-                    <div class="rating-stars">
-                        <i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i>
-                    </div>
-                    <a class="product-thumb" href="shop-single-2.html">
-                        <img src="/Home/images/shop/products/21.jpg" alt="Product">
-                    </a>
-                    <h3 class="product-title"><a href="shop-single-2.html">Acer Aspire 15.6 i3</a></h3>
-                    <h4 class="product-price">$649.50</h4>
-                    <div class="product-buttons">
-                        <div class="product-buttons">
-                            <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist">
-                                <i class="icon-heart"></i>
-                            </button>
-                            <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Product #6 -->
+       
         </div>
         <!-- End Related Products -->
     </div>
